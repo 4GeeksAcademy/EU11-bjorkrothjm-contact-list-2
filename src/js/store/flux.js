57cc7166633e36b481f97d12d795588b3a26e745
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-07-10 16:22:14 +0000

    Made Home non-static by addiding js functionality

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
+
+	const baseUrl = "https://assets.breatheco.de/apis/fake/contact/"
+
 	return {
 		store: {
 			demo: [
@@ -12,18 +15,73 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
+			],
+			contacts: [
+				{
+					full_name : "Dave Bradley",
+					email : "dave@gmail.com",
+					agenda_slug : "my_super_agenda",
+					address :"47568 NW 34ST, 33434 FL, USA",
+					phone :"7864445566"
+				},
+				{
+					full_name : "Dave Bradley",
+					email : "dave@gmail.com",
+					agenda_slug : "my_super_agenda",
+					address :"47568 NW 34ST, 33434 FL, USA",
+					phone :"7864445566"
+				}
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
+
+			getContacts: async () => {
+				
+				const getContactsUrl = baseUrl+"/agenda/{agenda_slug}" // UPDATE
+				try{
+					const response = await fetch(getContactsUrl);
+					if (!response.ok){
+						console.log(response.ok)
+					}else{
+						const jsonData = await response.json();
+						console.log(jsonData)
+						setStore({ contacts: jsonData });
+					}
+				}catch(error){
+					console.log(error);
+				}
+
+
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
+
+			addNewContact: () => {
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
+			},
+
+			deleteContact: () => {
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
+			},
+
+			updateContact: () => {
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
+			},
+
+
+
+			// Use getActions to call a function within a fuction
+			exampleFunction: () => {
+				getActions().changeColor(0, "green");
+			},
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
